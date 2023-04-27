1b0ab9521ab771646e39a163ef4e916dc419bbdf
doul <doulzang@gmail.com>
2023-04-27 17:54:50 +0900

    [04.27.]3.py수정

--- 3.py
+++ 3.py
@@ -4,14 +4,8 @@ sum=0
 team=['도울','지연','민지','세현','화진']
 
 for i in team:
-    own_score=int(input(f'[i]의 점수를 입력하세요:'))
-    score[i]=num
-
-score["도울"]=100
-score["지연"]=10
-score["민지"]=90
-score["세현"]=80
-score["화진"]=1
+    own_score=int(input(f'{i}의 점수를 입력하세요:'))
+    score[i]=own_score
 
 for v in score.values():
     sum+=v
